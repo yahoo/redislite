bb7fcb4e522b5a1f5a280dd1780481d68fce7e6b
Manu Phatak <bionikspoon@gmail.com>
2015-12-08 02:30:55 -0600

    restore sys.modules

--- redislite/client.py
+++ redislite/client.py
@@ -21,6 +21,7 @@ import signal
 import subprocess
 import tempfile
 import time
+import sys
 from . import configuration
 from . import __redis_executable__
 
@@ -61,12 +62,14 @@ class RedisMixin(object):
     redis_configuration = None
     redis_configuration_filename = None
 
-    def _cleanup(self):
+    def _cleanup(self, sys_modules=None):
         """
         Stop the redis-server for this instance if it's running
         :return:
         """
-        import psutil
+        if sys_modules:
+            import sys
+            sys.modules.update(sys_modules)
 
         if self.pid:
             logger.debug('Connection count: %s', self._connection_count())
@@ -313,7 +316,7 @@ class RedisMixin(object):
 
         logger.debug('Setting up redis with rdb file: %s', self.dbfilename)
         logger.debug('Setting up redis with socket file: %s', self.socket_file)
-        atexit.register(self._cleanup)
+        atexit.register(self._cleanup, sys.modules.copy())
         if self._is_redis_running() and not self.socket_file:
             self._load_setting_registry()
             logger.debug(
